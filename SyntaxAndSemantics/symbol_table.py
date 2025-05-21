a239983f974cef281b1583e4a20e46c6a76eeb22
NoHBruh <115785353+NoHBruh@users.noreply.github.com>
2025-05-21 10:38:28 +0200

    Create symbol_table.py

---
+++ SyntaxAndSemantics/symbol_table.py
@@ -0,0 +1,53 @@
+class SymbolTable :
+    def __init__(self): #equivalent of allocate function
+        self.scopes = [{}] #list of dictionnaries
+        
+    def insert(self, symbol, token_type,token_value, **kwargs) :
+        if symbol in self.scopes[-1] : # always checking the inner scope
+            raise Exception (f"{symbol} already in scope")
+        
+        sym = {"Type" :token_type,  "Value" : token_value}
+        sym.update(kwargs) #adding extra data when necessary, like tha array size
+        self.scopes[-1][symbol] = sym
+        
+        print(f"Added symbol: {symbol} -> {self.scopes[-1][symbol]}")
+     
+    def remove(self, symbol):
+        current = self.scope[-1]
+        if symbol in current :
+            del current[symbol]
+        else:
+            raise Exception(f"{symbol} has not been declared")
+               
+        
+    def update(self, symbol, Value = None, **kwargs) :
+        for scope in reversed(self.scopes) :
+            if symbol in scope :
+                scope[symbol].update({"Value" : Value})
+                print(f"symbol updated : {symbol} -> {scope[symbol]}")
+                return
+        raise Exception(f"{symbol} has not been declared")
+    
+    
+    def exist(self, symbol) :
+        return any(symbol in scope for scope in reversed(self.scopes))
+    
+    
+    def lookup(self, symbol) :
+        for scope in reversed(self.scopes) :
+            if symbol in scope :
+                return scope[symbol]
+        raise Exception(f"{symbol} has not been declared")
+    
+    
+    def new_scope(self) :
+        self.scopes.append({})
+        
+    
+    def leave_scope(self) :
+        if len(self.scopes) > 1 :
+            self.scopes.pop()
+        else :
+            raise Exception("Cannot leave global scope")
+        
+    
