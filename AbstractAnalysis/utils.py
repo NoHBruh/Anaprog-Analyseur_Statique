481be842514b1fdcde5c5e86de3eb061ea28f37b
NoHBruh <115785353+NoHBruh@users.noreply.github.com>
2025-07-05 21:18:46 +0200

    Update utils.py

--- AbstractAnalysis/utils.py
+++ AbstractAnalysis/utils.py
@@ -2,13 +2,13 @@ from abstractDomain import AbstractDomain
 def handle_arithmetic_abstract_value(var1, op, var2) :
     match op:
         case '+' :
-            if (var1 == AbstractDomain.POSITIVE == var2 == AbstractDomain.POSITIVE):
+            if (var1 == var2 == AbstractDomain.POSITIVE):
                 return AbstractDomain.POSITIVE
             
-            elif (var1 == AbstractDomain.ZERO == var2 == AbstractDomain.ZERO):
+            elif (var1 == var2 == AbstractDomain.ZERO):
                 return AbstractDomain.ZERO
             
-            elif (var1 == AbstractDomain.NEGATIVE == var2 == AbstractDomain.NEGATIVE):
+            elif (var1 == var2 == AbstractDomain.NEGATIVE):
                 return AbstractDomain.NEGATIVE
             
             else :
@@ -28,7 +28,7 @@ def handle_arithmetic_abstract_value(var1, op, var2) :
                 return AbstractDomain.UNSURE
             
         case '*' :
-            if (var1 == AbstractDomain.POSITIVE == var2 == AbstractDomain.POSITIVE or var1 == var2 == AbstractDomain.NEGATIVE):
+            if (var1  == var2 == AbstractDomain.POSITIVE or var1 == var2 == AbstractDomain.NEGATIVE):
                 return AbstractDomain.POSITIVE
             
             elif (var1 == AbstractDomain.NEGATIVE or var2 == AbstractDomain.NEGATIVE) :
