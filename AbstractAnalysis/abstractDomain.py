fadfb75b7431f799488eeac4cdaca99cb227ab65
NoHBruh <115785353+NoHBruh@users.noreply.github.com>
2025-07-05 20:03:33 +0200

    defined abstract domain and latice

---
+++ AbstractAnalysis/abstractDomain.py
@@ -0,0 +1,24 @@
+from enum import Enum
+from sys import maxsize
+
+class AbstractDomain(Enum) :
+    TOP = maxsize #maximum size of a data structure
+    NUMERIC = "N"
+    POSITIVE = "P"
+    ZERO = 0
+    NEGATIVE = "N"
+    UNSURE = "U" 
+    NOTNUMERIC = "NN" # booleans (float and decimals are not handled by SMALL)
+    BOTTOM = -maxsize #minimum value
+    
+    
+    """     Latice
+          -----------  
+               T
+            /    \
+           N       NN
+         / | \      /
+         P U  N    /
+         \ \  \   /
+                ⊥
+    """
