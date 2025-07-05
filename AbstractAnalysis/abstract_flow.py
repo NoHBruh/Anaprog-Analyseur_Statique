7e2cb09b2d735efd41d629184ce062286620b010
NoHBruh <115785353+NoHBruh@users.noreply.github.com>
2025-07-05 20:04:16 +0200

    conversion of flow function (WIP)

---
+++ AbstractAnalysis/abstract_flow.py
@@ -0,0 +1,74 @@
+from .abstractDomain import AbstractDomain
+from utils import handle_arithmetic_abstract_value
+
+class AbstractEnvironment :
+    def __init__(self):
+        self.abs_env = {}
+        
+        
+    """Abstract analysis will be made on array access and creation operations.
+    These operations need an index that can be a variable or a constant. 
+    We thus may pay attention to assignation and update (+ arithmetic) operations  on variables, as they could cause an IndexOutOfBoundsException
+      
+    FORWARD analysis using visitor pattern
+    """
+    
+    # ------FlowFunctions--------
+    
+    def constant_assign_flow_function(self, symbol, value):
+        """var x = constant c"""
+        if value in {True, False} :
+            self.abs_env[symbol] = AbstractDomain.NOTNUMERIC
+        elif (value < 0):
+            self.abs_env[symbol] = AbstractDomain.NEGATIVE
+            
+        else : 
+            self.abs_env[symbol] = value  #not abstract value, we can make an easy check on value if used as an array index
+        
+    
+    def variable_assign_flow_function(self, symbol, assigned_variable):
+        """ var symbol = var assigned_variable"""
+        if assigned_variable not in self.abs_env:
+            raise Exception("variable not in abstract environment")
+        
+        self.abs_env[symbol] = self.abs_env[assigned_variable]
+        print(f"Updated variable '{symbol}' in abstract environment to value of: {assigned_variable}")
+        
+    
+    
+    def variable_sum_assign_flow_function(self, symbol, assigned_variable1, assigned_variable2):
+        """var symbol = var assigned_variable1 + var assigned variable2"""
+        
+        if (assigned_variable1 not in self.abs_env or assigned_variable2 not in self.abs_env):
+            raise Exception("variable not in abstract environment")
+        
+        self.abs_env[symbol] = handle_arithmetic_abstract_value(self.abs_env[assigned_variable1], '+', self.abs_env[assigned_variable2])
+        
+        
+    def variable_sub_assign_flow_function(self, symbol, assigned_variable1, assigned_variable2):
+        """var symbol = var assigned_variable1 - var assigned variable2"""
+        
+        if (assigned_variable1 not in self.abs_env or assigned_variable2 not in self.abs_env):
+            raise Exception("variable not in abstract environment")
+        
+        self.abs_env[symbol] = self.abs_env[symbol] = handle_arithmetic_abstract_value(self.abs_env[assigned_variable1], '-', self.abs_env[assigned_variable2])    
+    
+    
+    def variable_mult_assign_flow_function(self, symbol, assigned_variable1, assigned_variable2):
+        """var symbol = var assigned_variable1 * var assigned variable2"""
+        
+        if (assigned_variable1 not in self.abs_env or assigned_variable2 not in self.abs_env):
+            raise Exception("variable not in abstract environment")
+        
+        self.abs_env[symbol] = self.abs_env[symbol] = handle_arithmetic_abstract_value(self.abs_env[assigned_variable1], '*', self.abs_env[assigned_variable2])  
+     
+        
+    def variable_div_assign_flow_function(self, symbol, assigned_variable1, assigned_variable2):
+        """var symbol = var assigned_variable1 / var assigned variable2"""
+        
+        if (assigned_variable1 not in self.abs_env or assigned_variable2 not in self.abs_env):
+            raise Exception("variable not in abstract environment")
+        
+        self.abs_env[symbol] = self.abs_env[symbol] = handle_arithmetic_abstract_value(self.abs_env[assigned_variable1], '/', self.abs_env[assigned_variable2])  
+    
+    
