684409f1cf441675e0540f89eb7391cce981f8a1
NoHBruh <115785353+NoHBruh@users.noreply.github.com>
2025-07-12 12:31:20 +0200

    Create main.py

---
+++ AbstractAnalysis/main.py
@@ -0,0 +1,26 @@
+from worklist import Worklist
+from lexer import get_lexer, tokenize
+from grammar_parser import get_parser
+
+
+def main():
+    with open("program.txt", 'r') as program_file :
+            code = program_file.read()
+
+    _lexer = get_lexer()
+    tokenize(_lexer, code)
+    print("-------------------------------------------------------------------------------")
+    parser = get_parser()
+    ast = parser.parse(code, debug=False)
+    import pprint
+    pprint.pprint(ast)
+    
+    wl = Worklist()
+    wl.visit(ast)
+    
+    wl.warnings.print_warnings()
+    
+    
+if __name__ == '__main__':
+    main()
+
