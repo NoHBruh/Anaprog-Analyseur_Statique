bf0fc2eb0954df74bc1bce1140dbb9b76f61d508
NoHBruh <115785353+NoHBruh@users.noreply.github.com>
2025-07-19 15:15:51 +0200

    Update abstract_flow.py

--- AbstractAnalysis/abstract_flow.py
+++ AbstractAnalysis/abstract_flow.py
@@ -32,7 +32,7 @@ class AbstractEnvironment :
         else :
             self.abs_env[symbol] = AbstractDomain.TOP
             
-        print(f'added symbol {symbol} with value {self.abs_env[symbol]} in abstract environment')
+        print(f'added/updated symbol {symbol} with value {self.abs_env[symbol]} in abstract environment')
         
     
     def variable_assign_flow_function(self, symbol, assigned_variable):
@@ -51,7 +51,7 @@ class AbstractEnvironment :
             raise Exception("variable not in abstract environment")
         
         self.abs_env[symbol] = handle_arithmetic_variables(self.abs_env[assigned_variable1], '+', self.abs_env[assigned_variable2])
-        
+        print(f'added/updated symbol {symbol} with value {self.abs_env[symbol]} in abstract environment')
         
     def variable_sub_assign_flow_function(self, symbol, assigned_variable1, assigned_variable2):
         """var symbol = var assigned_variable1 - var assigned variable2"""
@@ -60,7 +60,7 @@ class AbstractEnvironment :
             raise Exception("variable not in abstract environment")
         
         self.abs_env[symbol] = self.abs_env[symbol] = handle_arithmetic_variables(self.abs_env[assigned_variable1], '-', self.abs_env[assigned_variable2])    
-    
+        print(f'added/updated symbol {symbol} with value {self.abs_env[symbol]} in abstract environment')
     
     def variable_mult_assign_flow_function(self, symbol, assigned_variable1, assigned_variable2):
         """var symbol = var assigned_variable1 * var assigned variable2"""
@@ -69,7 +69,7 @@ class AbstractEnvironment :
             raise Exception("variable not in abstract environment")
         
         self.abs_env[symbol] = self.abs_env[symbol] = handle_arithmetic_variables(self.abs_env[assigned_variable1], '*', self.abs_env[assigned_variable2])  
-     
+        print(f'added/updated symbol {symbol} with value {self.abs_env[symbol]} in abstract environment')
         
     def variable_div_assign_flow_function(self, symbol, assigned_variable1, assigned_variable2):
         """var symbol = var assigned_variable1 / var assigned variable2"""
@@ -78,56 +78,64 @@ class AbstractEnvironment :
             raise Exception("variable not in abstract environment")
         
         self.abs_env[symbol] = self.abs_env[symbol] = handle_arithmetic_variables(self.abs_env[assigned_variable1], '/', self.abs_env[assigned_variable2])  
+        print(f'added/updated symbol {symbol} with value {self.abs_env[symbol]} in abstract environment')
     
     def var_const_sum_assign_flow_function(self, symbol, assigned_variable, constant) :
         if (assigned_variable not in self.abs_env):
             raise Exception("variable not in abstract environment")
         
         self.abs_env[symbol] = handle_arithmetic_with_constant_right(self.abs_env[assigned_variable], '+' , constant)
+        print(f'added/updated symbol {symbol} with value {self.abs_env[symbol]} in abstract environment')
         
     def var_const_sub_assign_flow_function(self, symbol, assigned_variable, constant) :
         if (assigned_variable not in self.abs_env):
             raise Exception("variable not in abstract environment")
         
         self.abs_env[symbol] = handle_arithmetic_with_constant_right(self.abs_env[assigned_variable], '-' , constant)
+        print(f'added/updated symbol {symbol} with value {self.abs_env[symbol]} in abstract environment')
+        
         
     def var_const_mult_assign_flow_function(self, symbol, assigned_variable, constant) :
         if (assigned_variable not in self.abs_env):
             raise Exception("variable not in abstract environment")
         
         self.abs_env[symbol] = handle_arithmetic_with_constant_right(self.abs_env[assigned_variable], '*' , constant)
-        
+        print(f'added/updated symbol {symbol} with value {self.abs_env[symbol]} in abstract environment')
         
     def var_const_div_assign_flow_function(self, symbol, assigned_variable, constant) :
         if (assigned_variable not in self.abs_env):
             raise Exception("variable not in abstract environment")
         
         self.abs_env[symbol] = handle_arithmetic_with_constant_right(self.abs_env[assigned_variable], '/' , constant)
-      
+        print(f'added/updated symbol {symbol} with value {self.abs_env[symbol]} in abstract environment')
+        
     def const_var_sum_assign_flow_function(self, symbol, constant, assigned_variable) :
         if (assigned_variable not in self.abs_env):
             raise Exception("variable not in abstract environment")   
         
         self.abs_env[symbol] = handle_arithmetic_with_constant_left(constant, '+' ,self.abs_env[assigned_variable])
+        print(f'added/updated symbol {symbol} with value {self.abs_env[symbol]} in abstract environment')
         
     def const_var_sub_assign_flow_function(self, symbol, constant, assigned_variable) :
         if (assigned_variable not in self.abs_env):
             raise Exception("variable not in abstract environment")   
         
         self.abs_env[symbol] = handle_arithmetic_with_constant_left(constant, '-' ,self.abs_env[assigned_variable])
+        print(f'added/updated symbol {symbol} with value {self.abs_env[symbol]} in abstract environment')
         
     def const_var_mult_assign_flow_function(self, symbol, constant, assigned_variable) :
         if (assigned_variable not in self.abs_env):
             raise Exception("variable not in abstract environment")   
         
         self.abs_env[symbol] = handle_arithmetic_with_constant_left(constant, '*' ,self.abs_env[assigned_variable])
+        print(f'added/updated symbol {symbol} with value {self.abs_env[symbol]} in abstract environment')
         
     def const_var_div_assign_flow_function(self, symbol, constant, assigned_variable) :
         if (assigned_variable not in self.abs_env):
             raise Exception("variable not in abstract environment")   
         
         self.abs_env[symbol] = handle_arithmetic_with_constant_left(constant, '/' ,self.abs_env[assigned_variable])
-        
+        print(f'added/updated symbol {symbol} with value {self.abs_env[symbol]} in abstract environment')
         
 #----------------ApplyingConditions-------------------------        
     def conditions_handler(self, conditions) :
@@ -221,12 +229,13 @@ class AbstractEnvironment :
 
     def join(self ,abs_env1, abs_env2):
         result = None
+        
         for var in abs_env1:
             if var in abs_env2 :
                 val1 = abs_env1[var]
                 val2 = abs_env2[var]
                 
-                if val1 not in AbstractDomain and val2 not in AbstractDomain :
+                if val1 not in AbstractDomain and val2 not in AbstractDomain and val1 != val2 :
                     result = handle_concrete_val_least_upper_bound(val1, val2)
 
                 elif val1 in AbstractDomain and val2 in AbstractDomain :
@@ -235,7 +244,34 @@ class AbstractEnvironment :
                 #only one value is abstract    
                 elif val1 in AbstractDomain != val2 in AbstractDomain : 
                     result = handle_var_const_least_upper_bound(val1, val2)
-
-                self.abs_env[var] = result
-                print(f'join operation done for variable {var} now with abstract value {result}')
+                
+                if result != None :
+                    self.abs_env[var] = result
+                    print(f'join operation done for variable {var} now with abstract value {result}')
+                
+    
+    def while_widening_handler(self, node) :
+        _, stmts_list = node
+        
+        for stmt in stmts_list :
+            match stmt :
+                case ('assign', var_id, value) :
+                    match value :
+                        case('arithExpr_binOp', _, op, _ ) :
+                            self.widening(var_id, op)
+        
+    def widening(self, symbol, op) :
+        match op:
+            case '+' |'*' :
+                self.abs_env[symbol] = AbstractDomain.TOP
             
+            case '-':
+                self.abs_env[symbol] = AbstractDomain.BOTTOM
+                
+            case '/' :
+                self.abs_env[symbol] = AbstractDomain.ZERO
+            
+            case _:
+                return    
+            
+        print(f' widening done, variable {symbol} now with abstract value {self.abs_env[symbol]}')
